17b73a729869549feb5a9b0b68e501e9396f33ad
sunlubo <1906653747@qq.com>
2017-02-10 14:23:47 +0800

    fix bug

--- TiCTacToe/TicTacToeViewController.swift
+++ TiCTacToe/TicTacToeViewController.swift
@@ -30,16 +30,16 @@ final class TicTacToeViewController: UIViewController {
         
         cellButtons.append(contentsOf: [button1, button2, button3, button4, button5, button6, button7, button8, button9])
         
-        reset()
+        resetButtonClicked()
     }
     
     // When the view tells us a cell is clicked in the tic tac toe board, this method will fire.
     // We update the model and then interrogate it's state to decide how to proceed.
     // If X or O won with this move, update the view to display this and otherwise mark the cell that was clicked.
-    @IBAction func buttonClicked(button: UIButton) {
+    @IBAction func cellsClicked(button: UIButton) {
         let tag = button.tag.description
-        let row = Int(tag.substring(to: tag.index(tag.startIndex, offsetBy: 1)))!
-        let col = tag.characters.count == 1 ? 0 : Int(tag.substring(with: tag.index(tag.startIndex, offsetBy: 1)..<tag.endIndex))!
+        let row = Int(tag.substring(to: tag.index(tag.startIndex, offsetBy: 1)))! - 1
+        let col = Int(tag.substring(with: tag.index(tag.startIndex, offsetBy: 1)..<tag.endIndex))! - 1
         
         if let player = model.mark(row: row, col: col) {
             button.setTitle(player.rawValue, for: .normal)
@@ -52,7 +52,7 @@ final class TicTacToeViewController: UIViewController {
     }
     
     // On reset, we clear the winner label and hide it, then clear out each button. We also tell the model to reset (restart) it's state.
-    @IBAction func reset() {
+    @IBAction func resetButtonClicked() {
         model.restart()
         
         cellButtons.forEach({ $0.setTitle(nil, for: .normal) })
